57ed20734ee733c189a6d23202aca8d95c92fc79
Katrin <alimova.katrin8@gmail.com>
2021-03-25 17:35:20 +0300

    unstable behavior for new todo

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <div id="form-container">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <Form v-on:addTodo="addTodo"/>
+    </div>
+
     <List :list="list" :switchStatus="switchStatus"/>
   </div>
 </template>
 
 <script>
-import List from './components/List.vue'
+import List from './components/List.vue';
+import Form from './components/Form';
 
 export default {
   name: 'App',
-  data(){
+  data () {
     return {
-    list:[{
-      title:'Walk granny',
-      completed: false,
-      id:0
-    }]
-    }
+      list: [ {
+        title: 'Walk granny',
+        completed: false,
+        id: 0
+      } ]
+    };
   },
-  methods:{
-    switchStatus(index){
-      this.list[index].completed = !this.list[index].completed
+  methods: {
+    switchStatus ( index ) {
+      this.list[index].completed = !this.list[index].completed;
+    },
+    addTodo ( title ) {
+      const todo = {
+        title,
+        completed: false,
+        id: this.list.length
+      };
+      this.list = [todo,...this.list];
     }
   },
   beforeCreate () {
     fetch('https://jsonplaceholder.typicode.com/todos')
         .then(response => response.json())
-        .then(json => this.list = [...this.list,...json])
+        .then(json => this.list = [ ...this.list, ...json ]);
   },
   components: {
-    List
+    List,
+    Form
   }
-}
+};
 </script>
 
 <style>
@@ -43,5 +57,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#form-container {
+  display: flex;
+  align-items: center;
+  justify-content: center /*flex-direction: ;*/
 }
 </style>
---
+++ src/components/Form.vue
@@ -0,0 +1,46 @@
+<template>
+  <form @submit.prevent="$emit('addTodo',title)">
+    <input v-model="title" name="title"/>
+    <button>
+      Add
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  emits: [ 'addTodo' ],
+  data () {
+    return {
+      title: null
+    };
+  }
+};
+</script>
+
+<style>
+form {
+  border-radius: 15px;
+  background-color: #2c3e50;
+  padding: 15px;
+}
+
+form > input {
+  border-radius: 5px;
+  padding: 5px;
+}
+
+form > button {
+  border-radius: 5px;
+  padding: 5px;
+  margin: 5px;
+  background-color: white;
+  transition: all 0.5s ease-in-out;
+}
+
+form > button:hover {
+  background-color: #5dc080;
+  transform: scale(1.1);
+}
+</style>
