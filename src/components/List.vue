bf29b67059f80d5ce15258072c4ce7da6d45c0f5
Katrin <alimova.katrin8@gmail.com>
2021-03-25 12:28:05 +0300

    List component,Todo comp

---
+++ src/components/List.vue
@@ -0,0 +1,14 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'List',
+  props: {
+    list: Array
+  },
+
+}
+</script>
+
---
+++ src/components/Todo.vue
@@ -0,0 +1,12 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  props: {
+    todo: Object
+  }
+};
+</script>
